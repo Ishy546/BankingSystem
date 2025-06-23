9dbb48da970e4f513769034b29ebe6e0fa457bd7
Ishy546 <172933946+Ishy546@users.noreply.github.com>
2025-06-23 15:24:06 -0400

    Implemented the SavingsAccount class

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,16 +1,32 @@
+import java.time.LocalDate;
+
 public abstract class BankAccount {
     private int accountNumber;
     private String accountHolderName;
     private double balance;
-    private String type;
+    private LocalDate dateCreated;
     public void deposit(double deposit){
         this.balance += deposit;
     }
     public double withDraw( double withdrawal){
-        balance -= withdrawal;
-        return withdrawal;
+        if (withdrawal<=balance){
+            balance -= withdrawal;
+            return withdrawal;
+        }else{
+            System.out.println("Withdrawal is more than what you have in your bank account!");
+        }
+        return null;
     }
-    public double checkingBalance(){
+    public double getBalance(){
         return this.balance;
+    }
+    public String getAccountDetails(){
+        return "Account Number: "+accountNumber+
+        "\nAccount Holder Name: "+ accountHolderName+
+        "\nDate Created: "+dateCreated;
+    }
+    public boolean validateAmount(double amount){
+        if (amount>0) return true;
+        return false;
     }
 }
--- src/SavingsAccount.java
+++ src/SavingsAccount.java
@@ -1,8 +1,12 @@
 public class SavingsAccount extends BankAccount {
     private double interestRate;
-    public SavingsAccount(int accountNumber, String accountHolderName, double balance, String type, int interestRate){
-        super(accountNumber, accountHolderName, balance, type);
+    public double minimumBalance;
+    public SavingsAccount(int accountNumber, String accountHolderName, double balance, int interestRate){
+        super(accountNumber, accountHolderName, balance);
         this.interestRate=interestRate;
+    }
+    public double calculateInterest(){
+        return getBalance()*0.035;
     }
 
 }
